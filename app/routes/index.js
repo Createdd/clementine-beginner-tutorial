c766f2f270500e6c1f6e4cea2b9997fd2c347765
Daniel Deutsch <deudan1010gmail.com>
2016-11-17 16:54:44 +0100

    simple node server and routes

---
+++ app/routes/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var express = require('express'),
+    routes = require('./app/routes/index.js');
+
+var app = express();
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 "use strict";// it enforces more secure syntax and best practices through stricter syntax constraints
-var express=require("express");
-var app=express();
+var express=require("express");//including Express as a dependency of our application
+var app=express();// initializing Express
 var port=process.env.PORT||3000;
 
 app.get("/", function(req, res){
-  res.send("It is working!!!!");
-})
+  res.sendFile(process.cwd()+"/index.html");//process.cwd() is like __dirname
+});//.get is a method that is going to take a request (req) from the client (browser) and respond (res) by sending  a message to the browser
 
 app.listen(port, function(){
   console.log("Listening to port: "+port);
-});
+});//tell Node which port to listen on
