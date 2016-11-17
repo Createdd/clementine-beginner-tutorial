c103c13b31ec94c49c82e46a229d2002c5b75e21
Daniel Deutsch <deudan1010gmail.com>
2016-11-17 17:21:44 +0100

    trying to connect to mongodb

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ var express=require("express");//including Express as a dependency of our applic
 var app=express();// initializing Express
 var port=process.env.PORT||3000;
 var routes = require('./app/routes/index.js');
+var mongo = require('mongodb').MongoClient;//requiring the MongoDB Node.js driver with - MongoClient() is an object that allows use to use functionality like connect. Initialize express before connecting to mongodb!!!
 
-app.use('/public', express.static(process.cwd() + '/public'));//bind the directory path for /public to a shortcut: /public
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+  if(err){
+    throw new Error("Database failed to connect!!!!");
+  } else {
+    console.log("MongoDB connected successfully!")
+  }
 
-routes(app);//allow us to use Express functionality within the scope of our new routes function
+  app.use('/public', express.static(process.cwd() + '/public'));//bind the directory path for /public to a shortcut: /public
+  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-/*app.get("/", function(req, res){
-  res.sendFile(process.cwd()+"/index.html");//process.cwd() is like __dirname
-});//.get is a method that is going to take a request (req) from the client (browser) and respond (res) by sending  a message to the browser*/
+  routes(app, db);//allow us to use Express functionality within the scope of our new routes function. to help pass data between the database on the client-side of our application
 
-app.listen(port, function(){
-  console.log("Listening to port: "+port);
-});//tell Node which port to listen on
+  app.listen(port, function(){
+    console.log("Listening to port: "+port);
+  });//tell Node which port to listen on
+});//connect to the MongoDB database using the connect method of the MongoClient object. - Port 27017 is the default port that MongoDB uses - clementinejs is the actual name of the database within MongoDB
